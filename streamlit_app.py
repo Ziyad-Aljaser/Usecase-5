2e21a8599e432b6b7f3b10c7c4e8195e24ad4579
Ziyad-Aljaser <120469041+Ziyad-Aljaser@users.noreply.github.com>
2024-08-11 21:22:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.write('Hello world!')
+
+st.image('Image1.png', caption='Screenshot 1')
+
+st.image('image2.png', caption='Screenshot 2')
+
+st.image('Image3.png', caption='Screenshot 3')
+
+st.image('Image4.png', caption='Screenshot 4')
+
