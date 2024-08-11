7ce6af4fccb6d5802d4262231d39f6fe9f245d59
Ziyad-Aljaser <120469041+Ziyad-Aljaser@users.noreply.github.com>
2024-08-11 22:22:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,31 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+
+st.markdown(
+    "<p>In the Kingdom, the distribution of job postings reveals some interesting trends. As depicted in chart below, Riyadh dominates the job market, accounting for a significant 42.8% of the opportunities, followed by Makkah, which holds 24.8% of the postings. Other regions and the Eastern Province also contribute to the job landscape, though to a lesser extent.</p>",
+    unsafe_allow_html=True
+)
 st.image('Image1.png', caption='Screenshot 1')
 
+
+st.markdown(
+    "<p>Moving forward, the chart below highlights the gender preferences associated with these job postings. It becomes apparent that while a substantial 39.5% of the jobs are open to both genders, there is still a noticeable bias, with 32.7% of positions favoring males and 27.8% favoring females.</p>",
+    unsafe_allow_html=True
+)
 st.image('image2.png', caption='Screenshot 2')
 
+
+st.markdown(
+    "<p>Delving into the financial aspect, the chart below provides insight into the expected salary range for fresh graduates. The majority of positions offer a starting salary of around 4,000 SAR, with only a few roles extending into higher salary brackets, reflecting the entry-level nature of these opportunities.</p>",
+    unsafe_allow_html=True
+)
 st.image('Image3.png', caption='Screenshot 3')
 
+
+st.markdown(
+    "<p>Lastly, this chart sheds light on the experience level sought by employers. A striking 800 job postings are aimed at fresh graduates with no prior experience, while experienced professionals are considered for just over 600 positions, underscoring the demand for fresh talent in the Kingdom’s job market.</p>",
+    unsafe_allow_html=True
+)
 st.image('Image4.png', caption='Screenshot 4')
 
